9975549417d2d2e764fbdf953ea3950b2fcd8c4c
Gene Hallman <genehallman@gmail.com>
2018-03-18 16:30:43 -0700

    added recipes and attributes

---
+++ spooky/metadata.rb
@@ -0,0 +1,11 @@
+name        "spooky"
+description "Installs Spooky"
+maintainer  "Gene"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'ruby'
+depends 'opworks_nodejs'
+depends 'opsworks_postgres'
+depends 'nginx'
+depends 'unicorn'
--- spooky/recipes/deploy.rb
+++ spooky/recipes/deploy.rb
@@ -1,1 +1,7 @@
 # deploy script here
+git "#{Chef::Config[:file_cache_path]}/spooky" do
+  repository node[:app_name][:git_repository]
+  revision node[:app_name][:git_revision]
+  action :sync
+end
+
