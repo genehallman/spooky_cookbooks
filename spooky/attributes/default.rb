9be39dee7ff2bda1dea64cf48d3275a1a2e06a04
Gene Hallman <genehallman@gmail.com>
2018-03-18 23:15:23 -0700

    more work

--- spooky/attributes/default.rb
+++ spooky/attributes/default.rb
@@ -1,4 +1,4 @@
-default["ruby-ng"][:ruby_version] = "2.4.1"
+default["ruby-ng"][:ruby_version] = "2.4"
 
 default[:spooky][:git_repository] = "https://github.com/chungkikelly/spooky/"
 default[:spooky][:git_revision] = "master"
