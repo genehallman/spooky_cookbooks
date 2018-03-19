3f6c163757ee85ffafa273d7a09a2cbb1d4d5ae5
Gene Hallman <genehallman@gmail.com>
2018-03-18 22:00:40 -0700

    more stuff

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,3 @@
-override[:languages][:ruby][:default_version] = "2.4.1"
-
-
 default[:spooky][:git_repository] = "https://github.com/chungkikelly/spooky/"
 default[:spooky][:git_revision] = "master"
+default[:spooky][:path] = "/opt/spooky"
--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,7 +1,23 @@
 # deploy script here
-git "#{Chef::Config[:file_cache_path]}/spooky" do
+git "#{node[:spooky][:path]}" do
   repository node[:spooky][:git_repository]
   revision node[:spooky][:git_revision]
   action :sync
 end
 
+  "ruby": {
+    "major_version": "2",
+    "minor_version": "3",
+    "patch_version": "4",
+    "pkgrelease": "1",
+    "full_version": "2.4",
+    "version": "2.4.1"
+  },
+  "opsworks_nodejs": {
+    "version": "6.10.1",
+    "pkgrelease": "1"
+  }
+
+opsworks_postgres
+nginx
+unicorn
