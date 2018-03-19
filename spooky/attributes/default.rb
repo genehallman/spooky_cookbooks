e5c1ac7de30aa045935e1a7703193027cf137591
Gene Hallman <genehallman@gmail.com>
2018-03-18 18:49:11 -0700

    more stuff

--- spooky/attributes/default.rb
+++ spooky/attributes/default.rb
@@ -1,4 +1,4 @@
-override[:languages[:ruby][:default_version] = "2.4.1"
+override[:languages][:ruby][:default_version] = "2.4.1"
 
 
 default[:spooky][:git_repository] = "https://github.com/chungkikelly/spooky/"
