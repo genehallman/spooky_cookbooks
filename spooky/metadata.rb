a35362063c09b0ec6dc81d5e591073313d4fb5f4
Gene Hallman <genehallman@gmail.com>
2018-03-18 23:29:50 -0700

    more work

--- spooky/metadata.rb
+++ spooky/metadata.rb
@@ -5,7 +5,7 @@ license     "Apache 2.0"
 version     "1.0.0"
 
 depends 'ruby-ng'
-# depends 'nodejs'
+depends 'nodejs'
 # depends 'postgres'
 # depends 'nginx'
 # depends 'unicorn'
--- spooky/recipes/setup.rb
+++ spooky/recipes/setup.rb
@@ -1,7 +1,7 @@
 # setup script here
 
 include_recipe "ruby-ng"
-# include_recipe "nodejs"
+include_recipe "nodejs"
 # include_recipe "postgres"
 # include_recipe "nginx"
 # include_recipe "unicorn"
