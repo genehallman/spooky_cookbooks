b57f0635a73ff7a0ccfa85f6a7dc9d386f472de3
Gene Hallman <genehallman@gmail.com>
2018-03-20 17:17:58 -0700

    more work

--- spooky/attributes/default.rb
+++ spooky/attributes/default.rb
@@ -7,16 +7,19 @@ default[:spooky][:git_repository] = "https://github.com/chungkikelly/spooky/"
 default[:spooky][:git_revision] = "master"
 default[:spooky][:path] = "/opt/spooky"
 
-default[:spooky][:rails_serve_static_files] = "true"
 default[:spooky][:rails_env] = "production"
 
 environment = {
-  "RAILS_SERVE_STATIC_FILES": default[:spooky][:rails_serve_static_files],
   "SECRET_KEY_BASE": default[:spooky][:secret_key_base],
   "DATABASE_URL": default[:spooky][:database_url],
-  "RAILS_ENV": default[:spooky][:rails_env],
-  "CDN_HOST": default[:spooky][:cdn_host]
+  "RAILS_ENV": default[:spooky][:rails_env]
 }
+
+if node[:spooky][:cdn_host].present? and node[:spooky][:jenkins_git_commit].present?
+  environment["CDN_HOST"] = "#{node[:spooky][:cdn_host]}/#{node[:spooky][:jenkins_git_commit]}/"
+else
+  environment["RAILS_SERVE_STATIC_FILES"] = "true"
+end
 
 env_str = environment.map {|k,v| "#{k}=#{v}"}.join(' ')
 
