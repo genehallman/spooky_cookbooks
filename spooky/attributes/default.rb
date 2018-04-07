f79e3200ce8ac31ab7b6a93645e3a424d5b3783b
Gene Hallman <genehallman@gmail.com>
2018-04-06 23:56:41 -0700

    version lock apt

--- spooky/attributes/default.rb
+++ spooky/attributes/default.rb
@@ -8,13 +8,13 @@ default[:spooky][:git_revision] = "master"
 default[:spooky][:path] = "/opt/spooky"
 
 default[:spooky][:rails_env] = "production"
+default[:spooky][:log_to_stdout] = "true"
 
 default[:spooky][:environment] = {
   "SECRET_KEY_BASE": node[:spooky][:secret_key_base],
   "DATABASE_URL": node[:spooky][:database_url],
-  "RAILS_ENV": node[:spooky][:rails_env]
+  "RAILS_ENV": node[:spooky][:rails_env],
+  "RAILS_LOG_TO_STDOUT": node[:spooky][:log_to_stdout]
 }
-
-# env_str = environment.map {|k,v| "#{k}=#{v}"}.join(' ')
 
 default[:spooky][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
